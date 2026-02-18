871f20cd718419b09a636b58d95b74cabeeefc9b
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-02-18 07:12:22 +0530

    refactor: update import paths

--- __tests__/division.test.js
+++ __tests__/division.test.js
@@ -1,4 +1,4 @@
-const division = require("./division.js");
+const division = require("../src/division.js");
 
 test("division 6 by 3", () => {
     expect(division(6, 3)).toBe(2);
--- __tests__/multiply.test.js
+++ __tests__/multiply.test.js
@@ -1,4 +1,4 @@
-const multiply = require("./multiply.js");
+const multiply = require("../src/multiply.js");
 
 test("multiply 2 and 3 to equal 6", () => {
     expect(multiply(2, 3)).toBe(6);
